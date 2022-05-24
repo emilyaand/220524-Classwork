102ba1dae9f8a3ee254f9df36ef01a3a709dcd9b
emilyaand <104448537+emilyaand@users.noreply.github.com>
2022-05-24 20:44:54 +0400

    Add files via upload

---
+++ Emi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _220524_Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                Name = "Emilya",
+                Surname = "And"
+
+             };
+
+            Student student = new Student
+            {
+                Name = "Ali",
+                Surname = "Aliyev"
+
+            }; student.GetFullName();
+  
+        }
+    }
+}
